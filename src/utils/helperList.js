43ae55793615b3de5e751220536a3908d6a0d0be
Rakesh-pilly <rakesh.mudhiraj882@gmail.com>
2021-09-11 00:40:17 +0530

    intial commit

---
+++ src/utils/helperList.js
@@ -0,0 +1,110 @@
+export const showcaseSkills = [
+  {
+    label: "Javascript",
+    rating: 71,
+  },
+  {
+    label: "Node JS",
+    rating: 81,
+  },
+  {
+    label: "React",
+    rating: 76,
+  },
+  {
+    label: "Mongo dB",
+    rating: 72,
+  },
+  {
+    label: "Redux",
+    rating: 73,
+  },
+  {
+    label: "Express",
+    rating: 74,
+  },
+  {
+    label: "HTML CSS",
+    rating: 85,
+  },
+];
+
+export const frontSkillsList = [
+  {
+    label: "HTML5",
+    src: "https://img.icons8.com/color/480/000000/html-5--v1.png",
+  },
+  {
+    label: "CSS3",
+    src: "https://img.icons8.com/color/480/000000/css3.png",
+  },
+  {
+    label: "Javascript",
+    src: "https://img.icons8.com/color/480/000000/javascript.png",
+  },
+  {
+    label: "React",
+    src: "https://img.icons8.com/color/480/000000/react-native.png",
+  },
+  {
+    label: "Redux",
+    src: "https://img.icons8.com/color/480/000000/redux.png",
+  },
+  {
+    label: "Bootstrap",
+    src: "https://i.ibb.co/zx0Tt9M/bootstrap.png",
+  },
+  {
+    label: "Github",
+    src: "https://img.icons8.com/fluent/480/000000/github.png",
+  },
+  {
+    label: "NPM",
+    src: "https://img.icons8.com/color/480/000000/npm.png",
+  }
+  
+];
+
+export const backSkillsList = [
+  {
+    label: "Node JS",
+    src: "https://img.icons8.com/color/480/000000/nodejs.png",
+  },
+  {
+    label: "Express JS",
+    src: "https://i.ibb.co/9smDM6j/pngegg.png",
+  },
+  {
+    label: "Mongo dB",
+    src: "https://i.ibb.co/ZVh01dm/rsz-mong2.png",
+  },
+];
+
+export const projectsList = [
+  {
+    label: " Booking.com",
+    imgSrc: "images/booking.png",
+    src: "#",
+    githubSrc: "https://github.com/Ramlala-Yadav-Git/Booking.com-clone",
+    description: "To clone Booking.com Booking.com is a Dutch start-up, that provides travel assistance like booking of hotels, resorts, flights, cars, and taxies etc. to the users across the world.",
+    stack: ["React", "javascript", "HTML", "CSS",],
+  },
+  {
+    label: "Instahrye",
+    imgSrc: "images/isntahyre.jpg",
+    src: "#",
+    githubSrc: "https://github.com/Rakesh-pilly/Instahyre",
+    description: "Clone of Instahrye, Instahyre is an advanced hiring platform based on artificial intelligence, enabling recruiters to hire top talent effortlessly.",
+    stack: ["React",  "CSS", "JavaScript", "HTML"],
+  },
+  {
+    label: "Groww",
+    imgSrc: "images/grow.jpg",
+    src: "#",
+    githubSrc: "https://github.com/Rakesh-pilly/project_grow",
+    description: "Clone of Groww, Groww is an online investment platform that allows investors to invest in mutual funds and stocks.",
+    stack: ["CSS", "JavaScript", "HTML"],
+  },
+  
+ 
+];
